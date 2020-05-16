c6c18f119f6fe41c363f21dcde1962e3e4c95b0a
Ladivcr <vrosas832@gmail.com>
2020-05-15 20:02:59 -0500

    update 15/05/2020

---
+++ 00runTrends.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/bin/python3 /home/ladiv/github/Sistemas-Distribuidos/01mainTuit.py
+/usr/bin/python3 /home/ladiv/github/Sistemas-Distribuidos/02storeDB.py
+/usr/bin/python3 /home/ladiv/github/Sistemas-Distribuidos/03dataProcessing.py
